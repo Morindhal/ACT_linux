e8a529981ac84033119e154c22bd549fb5f2d8eb
Morindhal <Morindhal@gmail.com>
2016-11-02 17:13:25 +0100

    Added code for more than one encounter in the programs main-loop.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::SeekFrom;
+use std::fmt;
+use std::borrow::BorrowMut;
+use std::any::Any;
 
 use std::{thread, time};
 
@@ -30,20 +34,34 @@ struct Attacker
     name: String
 }
 
+impl Attacker
+{
+    fn attack(&self, attack_data: regex::Captures)
+    {
+        self.attacks.borrow_mut().push(Attack{damage: attack_data.name("damage").unwrap().parse::<u64>().unwrap(), victim: String::from(attack_data.name("target").unwrap()), timestamp: String::from(attack_data.name("datetime").unwrap()), crit: String::from(attack_data.name("crittype").unwrap())});
+        println!("{}-{}-damage", self.name, self.final_damage);
+    }
+    
+    fn print(&self) -> String
+    {
+        format!("Name : {}  -- DPS:  {}k\n", self.name, self.final_damage/1000)
+    }
+}
+
 struct Encounter
 {
-    attackers: RefCell<Vec<Attacker>>,
+    attackers: Vec<Attacker>,
     encounter_start: String, //timestamp of when the fight started, get this from whatever starts the encounter
     encounter_duration: u64 //duration of the encounter in nanoseconds, divide by 1000 to get seconds
 }
 
 impl Encounter
 {
-    fn exists(&self, name:&str) -> bool
+    fn exists(self, name:&str) -> bool
     {
-        for i in 0..((self.attackers.borrow()).len())
+        for i in 0..(self.attackers).len()
         {
-            if ((self.attackers).borrow())[i].name == name
+            if ((self.attackers))[i].name == name
             {
                 return  true;
             }
@@ -51,20 +69,41 @@ impl Encounter
         return false;
     }
     
-    fn add_attacker(&self, attacker: &str)
+    fn attack(self, attack_data: regex::Captures)
     {
-        (self.attackers).borrow_mut().push(Attacker{attacks: RefCell::new(Vec::new()), final_damage: 0, final_healed: 0, name: String::from(attacker)});
+        
+        if !(self.exists(attack_data.name("attacker").unwrap()))
+        {
+            (self.attackers).push(Attacker{attacks: RefCell::new(Vec::new()), final_damage: 0, final_healed: 0, name: String::from(attack_data.name("attacker").unwrap())});
+        }
+        for i in 0..(self.attackers.len())
+        {
+            if self.attackers[i].name == attack_data.name("attacker").unwrap()
+            {
+                self.attackers[i].final_damage += attack_data.name("damage").unwrap().parse::<u64>().unwrap();
+                (self.attackers)[i].attack(attack_data);
+                break;
+            }
+        }
     }
-    
-    fn attack(&self, attack_data: regex::Captures)
+}
+
+impl fmt::Display for Encounter
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
     {
-        println!("{}",attack_data.name("attack").unwrap());
+        write!(f, "Encounter:\n");
+        for i in 0..((self.attackers).len())
+        {
+            write!(f, "{}", ((self.attackers))[i].print());
+        }
+        write!(f, "\nFight over")
     }
 }
 
 fn main()
 {
-    let f = File::open("/home/bergman/Documents/rust/EXP/eq2log_Shepherd.txt").unwrap();
+    let f = File::open("/media/bergman/Games/SteamLibrary/SteamApps/common/EverQuest 2/logs/Maj'Dul/eq2log_Shepherd.txt").unwrap();
     /*{
         Ok(file) => file,
         Err(e) => 
@@ -75,52 +114,62 @@ fn main()
 
     //Start a encounter, this code will be moved into the main loop when it works
     let mut encounters: Vec<Encounter> = Vec::new();
-    encounters.push(Encounter{ attackers: RefCell::new(Vec::new()), encounter_start: String::from("START"), encounter_duration : 0});
     
-    let re = Regex::new(r"\((?P<time>\d+)\)\[(?P<datetime>(\D|\d)+)\] (?P<attacker>\D+)(YOUR|'s) (?P<attack>\D+) ((multi attacks)|multi) (?P<target>\D+) for a (?P<crittype>\D+) of (?P<damage>\d+) (cold|heat|mental|arcane|poison|noxious) damage").unwrap();
+    let re = Regex::new(r"\((?P<time>\d+)\)\[(?P<datetime>(\D|\d)+)\] (?P<attacker>[A-Za-z]+)( |'s)(?P<attack>\D+) ((multi attacks)|hits|hit) (?P<target>\D+) (?P<crittype>\D+) (?P<damage>\d+) (?P<damagetype>[A-Za-z]+) damage").unwrap();
 
     let mut file = BufReader::new(&f);
+    file.seek(SeekFrom::End(-10));
     
     let mut buffer = String::new();
     let mut battle_timer = time::Instant::now();
+    let mut fightdone = true;
     'main: loop
     {
+        buffer.clear();
         if file.read_line(&mut buffer).unwrap() > 0
         {
             /*CHECK IF ATTACK
                 CHECK ATTACKER, IF NO ATTACKER EXISTS CREATE A NEW ONE <-- this is done by String-matching the ATTACKERS name
                     PARSE ATTACK INTO ENCOUNTERS.ATTACKERS.ATTACK <-- this is done by String-matching the ATTACKERS name
             SAME FOR HEAL*/
-            let temp = re.captures(buffer.as_str()) ;
-            match temp {None => {}, Some(cap) =>
+            match re.captures(buffer.as_str()) {None => {}, Some(cap) =>
             {
-                //look to see if the attacker already has a post, if so place the attack there, if not push a new attacker
-                if !encounters[0].exists(cap.name("attacker").unwrap())
+           /* println!("{}", cap.name("attacker").unwrap());
+            if cap.name("attacker").unwrap() == "YO"
+            {
+                println!("{}", buffer);
+            }*/
+               // println!("{}", battle_timer.elapsed().subsec_nanos());
+                if fightdone
                 {
-                    encounters[0].add_attacker(cap.name("attacker").unwrap());
+                    println!("New fight!");
+                    encounters.push(Encounter{ attackers: &mut *Vec::new(), encounter_start: String::from("START"), encounter_duration : 0});
+                    fightdone = false;
                 }
-                encounters[0].attack(cap);
-                //encounters[0].attackers.cap.name("datetime").unwrap()
+                encounters.last().unwrap().attack(cap);
+                encounters.last_mut().unwrap().encounter_duration += battle_timer.elapsed().subsec_nanos() as u64;
+                battle_timer = time::Instant::now();
             }};
-            //if buffer.find("attacks") != ;
-            encounters[0].encounter_duration += battle_timer.elapsed().subsec_nanos() as u64;
-            battle_timer = time::Instant::now();
         }
         else
         {
             thread::sleep(time::Duration::from_millis(100));
-            if battle_timer.elapsed() >= time::Duration::from_millis(3000)
+        }
+        if battle_timer.elapsed() >= time::Duration::from_millis(3000)
+        {
+            if !fightdone
             {
-                println!("New battle incoming, {:?} time has elapsed since the last one.", battle_timer.elapsed().as_secs());
+                println!("{}", encounters.last().unwrap());
+                fightdone = true;
             }
         }
-        buffer.clear();
     }
 }
 
 /*
 (1477272172)[Mon Oct 24 03:22:52 2016] YOUR Raging Whirlwind multi attacks Bonesnapper for a critical of 38996 cold damage.\r\n
 (1477272172)[Mon Oct 24 03:22:52 2016] Kabernet\'s Asylum multi attacks Bonesnapper for a critical of 36622 mental damage.\r\n
+(1477646415)[Fri Oct 28 11:20:15 2016] YOUR Extreme Advantage hits training dummy for 11978400 crushing damage.
 
 
 
